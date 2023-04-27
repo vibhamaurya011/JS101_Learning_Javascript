7d65a5e8cbaba09b1accce71cb2092b935407b71
vibhamaurya011 <vibhamaurya011@gmail.com>
2023-04-27 18:21:09 +0530

    Assignment Submission

--- Introduction_to_Javascript/problem7.js
+++
@@ -1,5 +0,0 @@
-function findX(X) {
-    // Write code here
-    let n = X*3;
-    console.log(10+n);
-}
--- Mathematical_Operator/problem17.js
+++
@@ -1,4 +0,0 @@
-function infinityStones(one, two, three, four, five, six) {
-    //Write code here
-    console.log((one)+(two*2)+(three*3)+(four*3)+(five*2)+(six*2))
-}
